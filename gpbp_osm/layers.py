5c384a71deb74a49f29d5df964188e3d1c028ac6
hazelalberts <116730245+hazelalberts@users.noreply.github.com>
2022-11-11 12:50:47 +0100

    Add Road Network Calculation

--- gpbp_osm/layers.py
+++ gpbp_osm/layers.py
@@ -49,3 +49,42 @@ class AdmArea:
 
     def compute_potential_fac(self, spacing: float) -> None:
         self.grid_gdf = generate_grid_in_polygon(spacing, self.geometry)
+        
+class RoadNetworkLayer():
+    def __init__(self):
+        pass
+
+    def calculate_isochrone_isodistance_OSM(self, trip_length, country, adm_area_name, level, network_type,
+                                            distance_type) -> None:
+        import geopandas as gpd
+        import matplotlib.pyplot as plt
+        import networkx as nx
+        import osmnx as ox
+        from descartes import PolygonPatch
+        from shapely.geometry import Point
+
+        adm_area = AdmArea(country, level)
+        adm_area.get_adm_area(adm_area_name)
+        G = ox.graph_from_polygon(adm_area.geometry, network_type, retain_all=False)
+
+        gdf_nodes = ox.graph_to_gdfs(G, edges=False)
+        x, y = gdf_nodes["geometry"].unary_union.centroid.xy
+        center_node = ox.distance.nearest_nodes(G, x[0], y[0])
+
+        # For isochrones distance="time", for isodistances distance = "distance"
+        #TODO: convert distance to time if distance_type is time, depends on network_type
+
+        subgraph = nx.ego_graph(G, center_node, radius=trip_length, distance=distance_type)
+        node_points = [Point((data["x"], data["y"])) for node, data in subgraph.nodes(data=True)]
+        bounding_poly = gpd.GeoSeries(node_points).unary_union.convex_hull
+
+        # Plot
+        fig, ax = ox.plot_graph(
+            G, show=False, close=False, edge_color="#999999", edge_alpha=0.2, node_size=0
+        )
+        patch = PolygonPatch(bounding_poly, fc="#59b9f2", ec="none", alpha=0.6, zorder=-1)
+        ax.add_patch(patch)
+        plt.show()
+
+    def calculate_isochrone_isodistance_Mapbox(self):
+        pass
