17cf89b2153b5e98512ff8931b321224e44242e5
Timo Maarse <tmaarse@users.noreply.github.com>
2024-05-23 14:48:11 +0200

    Display solution and statistics for specific budget (#15)

--- gpbp_app/main_page.py
+++ gpbp_app/main_page.py
@@ -16,6 +16,8 @@ import pycountry
 import folium
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
+from streamlit_plotly_events import plotly_events
 
 def fit_to_bounding_box( 
         folium_map : folium.Map,
@@ -158,10 +160,10 @@ with tab2:
         pot_fac_button = st.button("Compute potential locations")
         if pot_fac_button:
             st.session_state.adm_area.compute_potential_fac(st.session_state.spacing)
-            loc_gdf = st.session_state.adm_area.pot_fac_gdf
-            for i in range(0, len(loc_gdf)):
+            pot_fac_gdf = st.session_state.adm_area.pot_fac_gdf
+            for i in range(0, len(pot_fac_gdf)):
                 folium.CircleMarker(
-                    [loc_gdf.iloc[i]["latitude"], loc_gdf.iloc[i]["longitude"]],
+                    [pot_fac_gdf.iloc[i]["latitude"], pot_fac_gdf.iloc[i]["longitude"]],
                     color="red",
                     fill=True,
                     radius=2,
@@ -354,27 +356,88 @@ with tab5:
                             already_open = already_open,
                             budget_list = range(int(st.session_state.budget)), solver = solver 
                         )
-                        
-            # import pickle
-            # with open('results.pkl', 'wb') as file:
-            #     pickle.dump(results, file)
 
             pdf = pd.DataFrame()
+            pdf.index.name = "budget"
             sdf = pd.DataFrame()
+            sdf.index.name = "budget"
             for key in results.keys():
                 for col in results[key].keys():
                     df = pd.DataFrame.from_dict(results[key][col], orient='index')
-                    pdf[col] = df.value / pop_count.sum()
-                    sdf[col] = df.solution
-                        
-            # df = pd.DataFrame.from_dict(results, orient='index')
-            # df['coverage'] = df.value / pop_count.sum()
-            # st.write(df)
-            
-            fig, ax = plt.subplots()
-            pdf.plot(ax=ax,style='.-')
-            ax.set_xticks(pdf.index)
-            ax.set_title('Pareto')
-            st.pyplot(fig)
-                    
-            st.write(pdf)
+                    pdf["covered"] = df.value / pop_count.sum()
+                    sdf["solution"] = df.solution
+
+            st.session_state["results"] = {
+                "pdf": pdf,
+                "sdf": sdf,
+                "pop_count": pop_count,
+                "current": current,
+                "potential": potential
+            }
+
+        if "results" in st.session_state:
+            st.subheader("Results")
+
+            results = st.session_state["results"]
+            pdf = results["pdf"]
+            sdf = results["sdf"]
+            pop_count = results["pop_count"]
+            current = results["current"]
+            potential = results["potential"]
+
+            fig = px.line(pdf, title='Budget vs Population Covered')
+            fig.update_layout(
+                yaxis_title="population covered",
+                legend_title_text="",
+            )
+            clicked_points = plotly_events(fig, click_event=True)
+
+            if len(clicked_points) > 0:
+                point = clicked_points[0]
+                results["selected_budget"] = point["x"]
+
+            if "selected_budget" in results:
+                st.subheader("Details for selected budget")
+
+                selected_budget = results["selected_budget"]
+                fac_gdf = st.session_state.adm_area.fac_gdf
+                pot_fac_gdf = st.session_state.adm_area.pot_fac_gdf
+
+                col1, col2, col3 = st.columns(3)
+                with col1:
+                    st.metric("Existing facilities", fac_gdf.shape[0])
+                with col2:
+                    st.metric("Selected budget", selected_budget)
+                with col3:
+                    percentage = round(pdf.loc[selected_budget].sum().sum() * 100, 2)
+                    st.metric("Population covered", f"{percentage}%")
+
+                open_locations = sdf.loc[selected_budget].values[0]
+
+                map = folium.Map(
+                    location=(0, 0),
+                    zoom_start=1,
+                )
+
+                for location in open_locations:
+                    existing = location < len(fac_gdf)
+                    if existing:
+                        location_data = fac_gdf.loc[location]
+                    else:
+                        location_data = pot_fac_gdf.loc[location - len(fac_gdf)]
+
+                    folium.Marker(
+                        [location_data.latitude, location_data.longitude],
+                        icon=folium.Icon(
+                            color="blue" if existing else "darkpurple",
+                            icon="hospital-o" if existing else "question",
+                            prefix="fa",
+                        ),
+                    ).add_to(map)
+
+                map = fit_to_bounding_box(
+                    map, 
+                    *st.session_state.adm_area.geometry.bounds 
+                )
+
+                st_folium(map, use_container_width=True, height=500, key="result_map")
