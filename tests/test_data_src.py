f20c7040451b4e35523cb9cce5b3ee7cfcc1a9d9
anoukb27 <anoukbaake@gmail.com>
2025-01-20 14:43:23 +0100

    updated tests for get_admarea_mask, raster_to_df and osm_facilities, new tests for fb_data

--- tests/test_data_src.py
+++ tests/test_data_src.py
@@ -1,38 +1,30 @@
 from unittest.mock import MagicMock
-import geopandas as gpd
+import pandas as pd
 
 import pytest
 import numpy as np
+import rasterio
 
 from shapely.geometry import Polygon, MultiPolygon
-from gpbp import data_src, layers
-from gpbp.data_src import raster_to_df, get_admarea_mask, osm_facilities
+from gpbp.data_src import raster_to_df, get_admarea_mask, osm_facilities, fb_pop_data
 
 
-def test_init_admarea():
-    adm_area = layers.AdmArea("GREECE", level=0)
-    assert adm_area is not None
+pytest.fixture()
+def create_population_dataset(longitude, latitude, population):
+    """
+    Generates a dataset with three columns: latitude, longitude, and population.
+    Returns a pandas DataFrame with 10 rows.
+    """
+    df = pd.DataFrame(columns=[ "longitude", "latitude", "population"])
+    for item in range(6):
+        row = [
+            longitude[item],
+            latitude[item],
+            population[item]
+        ]
 
-
-@pytest.mark.skip(reason="legacy test")
-def test_world_pop():
-    adm_area = layers.AdmArea("GRC", level=0)
-    adm_area.get_adm_area("Greece")
-    df = data_src.world_pop_data(adm_area=adm_area)
-    est_pop = round(df["population"].sum() / 1000000, 1)
-    print(est_pop)
-    assert est_pop == 10.4
-
-
-@pytest.mark.skip(reason="legacy test")
-def test_fb_pop():
-    adm_area = layers.AdmArea("GRC", level=0)
-    adm_area.get_adm_area("Greece")
-    df = data_src.fb_pop_data(adm_area=adm_area)
-    est_pop = round(df["population"].sum() / 1000000, 1)
-    print(est_pop)
-    assert est_pop == 10.4
-
+        df.loc[item] = row
+    return df
 
 @pytest.fixture
 def multipolygon():
@@ -49,6 +41,13 @@ def polygon():
 
 
 @pytest.fixture
+def fake_raster_dataset():
+    # Create a fake raster array with some non-zero values
+    fake_raster_data = np.zeros((1, 100, 100), dtype=np.uint8)
+    fake_raster_data[0, 50:60, 50:60] = 1  # Simulate non-zero values for the mask
+    return fake_raster_data
+
+@pytest.fixture
 def mock_raster_datasetreader(mocker):
     mock_raster_dataset = mocker.MagicMock()
     mock_raster_dataset.crs = "EPSG:4326"
@@ -62,45 +61,6 @@ def mock_raster_datasetreader(mocker):
     ))
     mock_raster_dataset.read = MagicMock(side_effect=lambda band: np.random.rand(100, 100), dtype=np.uint8)
     return mock_raster_dataset
-
-@pytest.fixture
-def fake_raster_dataset():
-    # Create a fake raster array with some non-zero values
-    fake_raster_data = np.zeros((1, 100, 100), dtype=np.uint8)
-    fake_raster_data[0, 50:60, 50:60] = 1  # Simulate non-zero values for the mask
-    return fake_raster_data
-
-class TestGetAdmareaMask():
-    def test_get_admarea_mask(self, mocker, multipolygon, mock_raster_datasetreader, fake_raster_dataset):
-        mock_mask = mocker.patch('data_src.riomask.mask', return_value=(fake_raster_dataset, None))
-
-        adm_mask = get_admarea_mask(multipolygon, mock_raster_datasetreader)
-
-        # Assert that the result adm_mask is correct
-        expected_mask = fake_raster_dataset[0] > 0
-        assert adm_mask.shape == expected_mask.shape
-        assert np.array_equal(adm_mask, expected_mask)
-
-        # Verify that riomask.mask was called with correct parameters
-        mock_mask.assert_called_once_with(
-            mock_raster_datasetreader, [multipolygon], all_touched=True, crop=False
-        )
-
-
-class TestRastertoDF:
-    def test_raster_to_df(self, mocker, fake_raster_dataset, mock_raster_datasetreader, multipolygon):
-
-        mock_src = mocker.patch('data_src.rasterio.open', return_value=mock_raster_datasetreader)
-        mock_mask = mocker.patch('data_src.riomask.mask', return_value=(fake_raster_dataset, None))
-
-        df = raster_to_df(raster_fpath='fake_path.tif', mask_polygon=multipolygon)
-
-        assert df is not None
-        assert df.shape[1] == 3
-        assert mock_src.call_count == 1
-        assert mock_mask.call_count == 1
-        mock_src.assert_called_once_with('fake_path.tif')
-        mock_mask.assert_called_once_with(mock_raster_datasetreader, [multipolygon], all_touched=True, crop=False)
 
 
 @pytest.fixture
@@ -116,6 +76,102 @@ def mock_gdf(mocker, polygon):
     mock_gdf.get_level_values = MagicMock(return_value=["1"])
     return mock_gdf
 
+
+class TestGetAdmareaMask():
+    def test_get_admarea_mask_correct(self, mocker, multipolygon, mock_raster_datasetreader, fake_raster_dataset):
+        mock_mask = mocker.patch('data_src.riomask.mask', return_value=(fake_raster_dataset, None))
+
+        adm_mask = get_admarea_mask(multipolygon, mock_raster_datasetreader)
+
+        # Assert that the result adm_mask is correct
+        expected_mask = fake_raster_dataset[0] > 0
+        assert adm_mask.shape == expected_mask.shape
+        assert np.array_equal(adm_mask, expected_mask)
+
+        # Verify that riomask.mask was called with correct parameters
+        mock_mask.assert_called_once_with(
+            mock_raster_datasetreader, [multipolygon], all_touched=True, crop=False
+        )
+    def test_get_admarea_mask_empty(self):
+        with pytest.raises(AttributeError):
+            get_admarea_mask(MultiPolygon([]), None)
+
+
+class TestRastertoDF:
+    def test_raster_to_df_correct(self, mocker, fake_raster_dataset, mock_raster_datasetreader, multipolygon):
+
+        mock_src = mocker.patch('data_src.rasterio.open', return_value=mock_raster_datasetreader)
+        mock_mask = mocker.patch('data_src.riomask.mask', return_value=(fake_raster_dataset, None))
+
+        df = raster_to_df(raster_fpath='fake_path.tif', mask_polygon=multipolygon)
+
+        assert df is not None
+        assert df.shape[1] == 3
+        assert mock_src.call_count == 1
+        assert mock_mask.call_count == 1
+        mock_src.assert_called_once_with('fake_path.tif')
+        mock_mask.assert_called_once_with(mock_raster_datasetreader, [multipolygon], all_touched=True, crop=False)
+
+    def test_raster_to_df_false_input_path(self):
+        with pytest.raises(rasterio.errors.RasterioIOError):
+            raster_to_df(raster_fpath='fake_path', mask_polygon=None)
+
+    def test_raster_to_df_empty_polygon(self, mocker, mock_raster_datasetreader):
+        mock_src = mocker.patch('data_src.rasterio.open', return_value=mock_raster_datasetreader)
+
+        with pytest.raises(IndexError):
+            raster_to_df(raster_fpath='fake_path.tif', mask_polygon=MultiPolygon([]))
+
+
+class TestFBdata:
+    def test_fb_data_mask_and_dataset_same(self, mocker, multipolygon):
+        longitude = [67.36, 67.11, 67.14, 66.50, 66.49, 66.48]
+        latitude = [36.29, 36.00, 35.96, 36.90, 36.91, 36.92]
+        population = [100, 200, 300, 400, 500, 600]
+        data = create_population_dataset(longitude, latitude, population)
+
+        mocker.patch("data_src.Resource.search_in_hdx", return_value=[{'id': 'fake_id', 'download_url': 'fake_url'}])
+        mocker.patch("data_src.urllib.request.urlretrieve", return_value= ('fakehandle.csv', None))
+        mocker.patch("data_src.pd.read_csv", return_value=data)
+        fb_data = fb_pop_data('AFG', multipolygon)
+
+        assert fb_data is not None
+        assert fb_data.shape[0] == 6
+        assert fb_data.shape[1] == 3
+        assert fb_data['population'].sum() == 2100
+
+    def test_fb_data_mask_and_dataset_partial_overlap(self, mocker, multipolygon):
+        longitude = [67.36, 67.11, 67.14, 54.13, 54.89, 53.45,]
+        latitude = [36.29, 36.00, 35.96, 22.13, 23.54, 22.63]
+        population = [100, 200, 300, 400, 500, 600]
+        data = create_population_dataset(longitude, latitude, population)
+
+        mocker.patch("data_src.Resource.search_in_hdx", return_value=[{'id': 'fake_id', 'download_url': 'fake_url'}])
+        mocker.patch("data_src.urllib.request.urlretrieve", return_value= ('fakehandle.csv', None))
+        mocker.patch("data_src.pd.read_csv", return_value=data)
+        fb_data = fb_pop_data('AFG', multipolygon)
+
+        assert fb_data is not None
+        assert fb_data.shape[0] == 3
+        assert fb_data.shape[1] == 3
+        assert fb_data['population'].sum() == 600
+
+    def test_fb_data_mask_and_dataset_no_overlap(self, mocker, multipolygon):
+        longitude = [54.13, 54.89, 53.45, 53.78, 54.09, 53.94]
+        latitude = [22.13, 23.54, 22.63, 22.87, 23.02, 22.98]
+        population = [100, 200, 300, 400, 500, 600]
+        data = create_population_dataset(longitude, latitude, population)
+
+        mocker.patch("data_src.Resource.search_in_hdx", return_value=[{'id': 'fake_id', 'download_url': 'fake_url'}])
+        mocker.patch("data_src.urllib.request.urlretrieve", return_value= ('fakehandle.csv', None))
+        mocker.patch("data_src.pd.read_csv", return_value=data)
+        fb_data = fb_pop_data('AFG', multipolygon)
+
+        assert fb_data is not None
+        assert fb_data.shape[0] == 0
+        assert fb_data.shape[1] == 3
+
+
 class TestOSMFacilities:
     def test_osm_facilities(self, mocker, mock_gdf, multipolygon):
         mocker.patch('data_src.ox.geometries_from_polygon', return_value=mock_gdf)
@@ -123,4 +179,12 @@ class TestOSMFacilities:
         gdf = osm_facilities('test_country', multipolygon, {"amenity": ["hospital", "clinic"]})
 
         assert gdf is not None
-        assert gdf.shape[1] == 4
+        assert gdf.shape[1] == 5
+
+    def test_osm_faciltiies_empty_multipolygon(self):
+        with pytest.raises(ValueError):
+            osm_facilities('test_country', MultiPolygon([]), {"amenity": ["hospital", "clinic"]})
+
+
+
+
