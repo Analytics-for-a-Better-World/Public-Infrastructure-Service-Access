e1f117ba377ef31d2ded9524f42a41590bda3a8e
Catalina <dcossaogh@use.startmail.com>
2025-02-19 15:52:31 +0100

    refactor: create_polygon_from_nodes_and_edges written and tested
    testing get_poly_nx bbefore refactoring it
    
    tests for old function done, ready to refactor

--- pisa/isopolygons.py
+++ pisa/isopolygons.py
@@ -1,7 +1,11 @@
 from abc import ABC, abstractmethod
 
+import geopandas as gpd
+import networkx as nx
+import osmnx as ox
 from networkx import MultiDiGraph
 from pandas import DataFrame
+from shapely import LineString, Point, Polygon
 
 
 class IsopolygonCalculator(ABC):
@@ -32,11 +36,104 @@ class OsmIsopolygonCalculator(IsopolygonCalculator):
         distance_type: str,
         distance_values: list[int],
         road_network: MultiDiGraph,
+        node_buff: float = 0.001,  # TODO: replace with buffer = 50 m
+        edge_buff: float = 0.0005,  # TODO: remove
     ):
         super().__init__(facilities_lat_lon, distance_type, distance_values)
         self.road_network = road_network
 
     def calculate_isopolygons(self) -> dict[DataFrame]: ...
+
+    @staticmethod
+    def create_polygon_from_nodes_and_edges(nodes_gdf, edges_gdf, node_buff, edge_buff):
+        """This method should be replaced with use osmnx.utils_geo.buffer_geometry(geom, dist)
+        where dist is 50m, as directed by Joaquim"""
+
+        if edge_buff <= 0:
+            raise ValueError("The parameter edge_buff must be greater than 0.")
+
+        # creates a circle with radius node_buff around each node
+        disks = nodes_gdf.buffer(node_buff).geometry
+
+        # creates a buffer around each edge. For example, if the edge is a line between
+        # two points, it creates a sort of 2-d "cylinder" of "radius" edge_buff
+        # and semicircles of radius edge_buff at the end points
+        cylinders = edges_gdf.buffer(edge_buff).geometry
+
+        all_geometries = list(disks) + list(cylinders)
+
+        # Merges all_geometries into a single unified geometry, removing overlaps and simplifying the resulting shape
+        geometric_union = gpd.GeoSeries(all_geometries).unary_union
+
+        # Removes any interior boundaries (holes) that geometric_union might have had
+        buffer_polygon = Polygon(geometric_union.exterior)
+
+        return buffer_polygon
+
+    @staticmethod
+    def _get_poly_nx_new(
+        road_network: nx.MultiDiGraph,
+        center_node: int,
+        dist_value: int,
+        distance_type: str,
+    ) -> tuple[gpd.GeoSeries, gpd.GeoSeries]:
+        """
+        Get nodes and edges within a specified distance from a certain node in a road network.
+
+        Parameters:
+            road_network (nx.MultiDiGraph): The road network.
+            center_node (int): The node from which to measure the distance.
+            dist_value (int): The distance value.
+            distance_type (str): The type of distance (e.g., 'length').
+
+        Returns:
+            nodes_gdf: a GeoSeries of the nodes with their osmid and geometry.
+            edges_gdf: a GeoSeries of the geometry of the edges.
+
+        If an edge (u,v) doesn't have geometry data in G, edges_gdf contains
+        a straight line from u to v.
+
+        Raises:
+            ValueError if all other nodes are farther than dist_value from center_node
+
+        """
+        subgraph = nx.ego_graph(
+            road_network, center_node, radius=dist_value, distance=distance_type
+        )
+
+        nodes_gdf, edges_gdf = ox.graph_to_gdfs(subgraph)
+
+        return nodes_gdf.loc[:, "geometry"], edges_gdf.loc[:, "geometry"].reset_index()
+
+    @staticmethod
+    def _get_poly_nx(
+        road_network: nx.MultiDiGraph, center_node, dist_value, distance_type
+    ) -> tuple[gpd.GeoDataFrame, gpd.GeoSeries]:
+        """
+        todo: change to native function if possible
+        todo: Rename
+        """
+
+        subgraph = nx.ego_graph(
+            road_network, center_node, radius=dist_value, distance=distance_type
+        )
+
+        node_points = [
+            Point((data["x"], data["y"])) for node, data in subgraph.nodes(data=True)
+        ]
+        nodes_gdf = gpd.GeoDataFrame({"id": list(subgraph.nodes)}, geometry=node_points)
+        nodes_gdf = nodes_gdf.set_index("id")
+
+        edge_lines = []
+        for n_fr, n_to in subgraph.edges():
+            f = nodes_gdf.loc[n_fr].geometry
+            t = nodes_gdf.loc[n_to].geometry
+            edge_lookup = road_network.get_edge_data(n_fr, n_to)[0].get(
+                "geometry", LineString([f, t])
+            )
+            edge_lines.append(edge_lookup)
+        edges_gdf = gpd.GeoSeries(edge_lines)
+        return nodes_gdf, edges_gdf
 
 
 class MapboxIsopolygonCalculator(IsopolygonCalculator):
--- tests/test_isopolygons_osm.py
+++ tests/test_isopolygons_osm.py
@@ -1,12 +1,68 @@
 import geopandas as gpd
-import networkx as nx
 import osmnx as ox
 import pandas as pd
 import pytest
+from geopandas.testing import assert_geodataframe_equal, assert_geoseries_equal
+from pandas import testing as tm
+from shapely.geometry import LineString, Point
 
 from pisa.isopolygons import OsmIsopolygonCalculator
 
-# I need a facilities_lat_lon gdf and a road_network
+
+@pytest.fixture
+def nodes_gdf() -> gpd.GeoSeries:
+
+    data = {
+        "osmid": [5909483625, 5909483619, 5909483636],
+        "geometry": [
+            Point(-122.231243, 37.7687576),
+            Point(-122.2314069, 37.7687054),
+            Point(-122.2317839, 37.7689584),
+        ],
+    }
+    return gpd.GeoSeries(data["geometry"], index=data["osmid"], crs="EPSG:4326")
+
+
+@pytest.fixture
+def nodes_geodataframe() -> gpd.GeoDataFrame:
+    data = {
+        "id": [5909483625, 5909483619, 5909483636],
+        "geometry": [
+            Point(-122.231243, 37.7687576),
+            Point(-122.2314069, 37.7687054),
+            Point(-122.2317839, 37.7689584),
+        ],
+    }
+    return gpd.GeoDataFrame(data).set_index("id")
+
+
+@pytest.fixture
+def edges_gdf() -> gpd.GeoSeries:
+    coordinates_25_to_19 = [(-122.23124, 37.76876), (-122.23141, 37.76871)]
+
+    coordinates_19_to_36 = [
+        (-122.2314069, 37.7687054),
+        (-122.2314797, 37.7687656),
+        (-122.2315618, 37.7688239),
+        (-122.2316698, 37.7688952),
+        (-122.2317839, 37.7689584),
+    ]
+
+    return gpd.GeoSeries(
+        [
+            LineString(coordinates_25_to_19),  # edge 5909483625 -> 5909483619
+            LineString(coordinates_25_to_19[::-1]),  # edge 5909483619 -> 5909483625
+            LineString(coordinates_19_to_36),  # edge 5909483619 -> 5909483636
+            LineString(coordinates_19_to_36[::-1]),  # edge 5909483636 -> 5909483619
+        ],
+        crs="EPSG:4326",
+    )
+
+
+@pytest.fixture
+def excluded_node() -> Point:
+    """Geometry of the node 5909483569"""
+    return Point(-122.2315948, 37.768278)
 
 
 @pytest.fixture
@@ -20,15 +76,178 @@ def dataframe_with_lat_and_lon() -> pd.DataFrame:
     return pd.DataFrame(points, columns=["longitude", "latitude"])
 
 
-def test_calculate_isopolygons():
-    
-    isopolygon_calculator = OsmIsopolygonCalculator(
-        facilities_lat_lon=dataframe_with_lat_and_lon,
-        distance_type="length",
-        distance_values=[10, 20, 50],
-        road_network=ox.load_graphml(
+# class TestOsmCalculateIsopolygons:
+
+#     @pytest.fixture(autouse=True)
+#     def setup(
+#         self,
+#         dataframe_with_lat_and_lon,
+#     ):
+#         """
+#         Here I'm deliberately choosing to override the defaults for node_buff and edge_buff
+#         so the tests pass, otherwise the tests would fail because the buffers are too large
+#         """
+
+#         self.isopolygons = OsmIsopolygonCalculator(
+#             facilities_lat_lon=dataframe_with_lat_and_lon,
+#             distance_type="length",
+#             distance_values=[5, 20, 50],
+#             road_network=ox.load_graphml(
+#                 "tests/test_data/walk_network_4_nodes_6_edges.graphml"
+#             ),
+#             node_buff=0.00005,
+#             edge_buff=0.00005,
+#         ).calculate_isopolygons()
+
+#     def test_format(self):
+#         assert self.isopolygons.shape == (
+#             2,
+#             3,
+#         ), "The output should have two rows (one per point in road_nodes) and three columns (one per distance)"
+
+#         assert list(self.isopolygons.columns) == ["ID_5", "ID_20", "ID_50"]
+
+#         # Node 5909483636 was kicked out in .nearest_nodes()
+#         assert list(self.isopolygons.index) == [5909483619, 5909483625]
+
+
+class TestCreatePolygonFromNodesAndEdges:
+
+    def test_excluded_node_is_left_out(self, nodes_gdf, edges_gdf, excluded_node):
+        """Desired behavior: the node previously excluded because it was
+        too far away is excluded from the resulting polygon"""
+
+        poly = OsmIsopolygonCalculator.create_polygon_from_nodes_and_edges(
+            nodes_gdf=nodes_gdf,
+            edges_gdf=edges_gdf,
+            node_buff=0.00005,
+            edge_buff=0.00005,
+        )
+
+        assert not poly.contains(excluded_node)
+
+    def test_excluded_node_is_back_in(self, nodes_gdf, edges_gdf, excluded_node):
+        """Undesired behavior: the node previously excluded because it was
+        too far away is included in the resulting polygon. The problem is
+        that buffers are too large.
+
+        These buffers are the default in gpbp/distance.py"""
+
+        poly = OsmIsopolygonCalculator.create_polygon_from_nodes_and_edges(
+            nodes_gdf=nodes_gdf,
+            edges_gdf=edges_gdf,
+            node_buff=0.001,
+            edge_buff=0.0005,
+        )
+
+        assert poly.contains(excluded_node)
+
+    def test_with_0_node_buffer(self, nodes_gdf, edges_gdf):
+        """This should not be a problem because all nodes are connected"""
+
+        poly = OsmIsopolygonCalculator.create_polygon_from_nodes_and_edges(
+            nodes_gdf=nodes_gdf, edges_gdf=edges_gdf, node_buff=0, edge_buff=0.00005
+        )
+
+        assert poly.area > 0
+
+
+class TestGetPolyNxNew:
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        """All tests use the same graph"""
+        self.road_network = ox.load_graphml(
             "tests/test_data/walk_network_4_nodes_6_edges.graphml"
-        ),
-    )
+        )
+
+    def test_get_poly_nx_nodes(self, nodes_gdf):
+        """Tests that the nodes are correct"""
+
+        (actual_nodes_gdf, _) = OsmIsopolygonCalculator._get_poly_nx_new(
+            self.road_network,
+            center_node=5909483619,
+            dist_value=50,
+            distance_type="length",
+        )
+
+        assert_geoseries_equal(actual_nodes_gdf, nodes_gdf)
+
+    def test_get_poly_nx_edges(self, edges_gdf):
+        """Tests that the geometry of the edges is correct"""
+
+        # we expect this edge to be discarded as its lenght is larger than 50
+        assert self.road_network.edges[(5909483619, 5909483569, 0)]["length"] > 50
+
+        (_, actual_edges_gdf) = OsmIsopolygonCalculator._get_poly_nx_new(
+            self.road_network,
+            center_node=5909483619,
+            dist_value=50,
+            distance_type="length",
+        )
+
+        assert actual_edges_gdf.geom_equals_exact(edges_gdf, tolerance=4).all()
+
+    def test_raises_value_error_if_all_nodes_are_too_far(self):
+
+        # TODO: this should actually just return the node itself
+
+        """All nodes are farther than 15 meters away from node 5909483619"""
+        with pytest.raises(ValueError, match="Graph contains no edges"):
+            OsmIsopolygonCalculator._get_poly_nx_new(
+                self.road_network,
+                center_node=5909483619,
+                dist_value=15,
+                distance_type="length",
+            )
 
 
+class TestGetPolyNxOld:
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        """All tests use the same graph"""
+        self.road_network = ox.load_graphml(
+            "tests/test_data/walk_network_4_nodes_6_edges.graphml"
+        )
+
+    def test_get_poly_nx_nodes(self, nodes_geodataframe):
+        """Tests that the nodes are correct"""
+
+        (actual_nodes_gdf, _) = OsmIsopolygonCalculator._get_poly_nx(
+            self.road_network,
+            center_node=5909483619,
+            dist_value=50,
+            distance_type="length",
+        )
+
+        assert_geodataframe_equal(actual_nodes_gdf, nodes_geodataframe)
+
+    def test_get_poly_nx_edges(self, edges_gdf):
+        """Tests that the geometry of the edges is correct"""
+
+        # we expect this edge to be discarded as its lenght is larger than 50
+        assert self.road_network.edges[(5909483619, 5909483569, 0)]["length"] > 50
+
+        (_, actual_edges_gdf) = OsmIsopolygonCalculator._get_poly_nx(
+            self.road_network,
+            center_node=5909483619,
+            dist_value=50,
+            distance_type="length",
+        )
+
+        assert actual_edges_gdf.geom_equals_exact(edges_gdf, tolerance=4).all()
+
+    def test_returns_center_node_if_all_others_are_too_far(self):
+        """All nodes are farther than 15 meters away from node 5909483619,
+        so the function returns the center node itself and no edges"""
+
+        (actual_nodes_gdf, actual_edges_gdf) = OsmIsopolygonCalculator._get_poly_nx(
+            self.road_network,
+            center_node=5909483619,
+            dist_value=15,
+            distance_type="length",
+        )
+
+        assert actual_nodes_gdf.shape[0] == 1  # only the center node
+        assert actual_edges_gdf.empty
