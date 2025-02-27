84fe34c9b6065c2f3965c7ed0d3e7d8b267f962a
Catalina <16209803+dcossao@users.noreply.github.com>
2025-02-27 19:01:30 +0100

    refactor: create osm isopolygon calculator class (#60)
    Co-authored-by: Catalina <dcossaogh@use.startmail.com>

---
+++ pisa/isopolygons.py
@@ -0,0 +1,338 @@
+"""
+This module provides functionality for calculating isopolygons around facilities using different methods and services.
+
+An isopolygon represents the area that can be reached within a specific distance (isodistance) or time (isochrone) from a facility.
+
+The module contains an abstract base class IsopolygonCalculator and its implementations.
+
+Note:
+    To implement a new way of calculating isopolygons (e.g., using Google Maps),
+    create a class that inherits from IsopolygonCalculator and implements calculate_isopolygons.
+
+"""
+
+import logging
+from abc import ABC, abstractmethod
+
+import geopandas as gpd
+import networkx as nx
+import osmnx as ox
+import pandas as pd
+from geopandas import GeoSeries
+from networkx import MultiDiGraph
+from pandas import DataFrame
+from shapely import Polygon
+
+
+class IsopolygonCalculator(ABC):
+    """Abstract base class for isopolygon calculation."""
+
+    def __init__(
+        self,
+        facilities_lon_lat: DataFrame,
+        distance_type: str,  # e.g. travel_time or length
+        distance_values: list[int],
+    ):
+
+        self.facilities_longitude_array = facilities_lon_lat.longitude.values
+        self.facilities_latitude_array = facilities_lon_lat.latitude.values
+        self.distance_type = distance_type
+        self.distance_values = distance_values
+
+        self._validate_input()
+
+    def _validate_input(self) -> None:
+        """Checks that distance values are within the permitted limits:
+        100.000 meters for length and 60 minutes for time."""
+
+        if self.distance_type == "length" and max(self.distance_values) > 100000:
+            raise ValueError(
+                "One or more distance values are larger than the permitted 100.000 meters limit."
+            )
+
+        if self.distance_type == "minutes" and max(self.distance_values) > 60:
+            raise ValueError(
+                "One or more distance values are larger than the permitted 60 minutes limit."
+            )
+
+    @abstractmethod
+    def calculate_isopolygons(self) -> DataFrame:
+        """Must be implemented in subclasses"""
+        pass
+
+
+class OsmIsopolygonCalculator(IsopolygonCalculator):
+    """This implementation of IsopolygonCalculator uses OpenStreetMap data to calculate isopolygons."""
+
+    def __init__(
+        self,
+        facilities_lon_lat: DataFrame,
+        distance_type: str,
+        distance_values: list[int],
+        road_network: MultiDiGraph,
+        node_buffer: float = 0.001,
+        edge_buffer: float = 0.0005,
+    ):
+        super().__init__(facilities_lon_lat, distance_type, distance_values)
+        self.road_network = road_network
+        self.node_buff = node_buffer
+        self.edge_buff = edge_buffer
+
+        # Find the nearest node in the road network for each facility
+        self.nearest_nodes = ox.distance.nearest_nodes(
+            G=self.road_network,
+            X=self.facilities_longitude_array,
+            Y=self.facilities_latitude_array,
+        )
+
+    def calculate_isopolygons(self) -> DataFrame:
+        """Calculates isopolygons for each facility at different distances (distance_values).
+
+        An isopolygon represents the area that can be reached within a specific distance/time from a facility
+        using the road network.
+
+        N.B.: distances will be computed with respect to the nearest node to the facility,
+        not the facility itself."""
+
+        index = pd.Index(self.nearest_nodes, name="nearest_node")
+        columns = [f"ID_{d}" for d in self.distance_values]
+        isopolygons = DataFrame(index=index, columns=columns)
+
+        # Construct isopolygon for each distance value
+        for distance_value in self.distance_values:
+
+            for road_node in self.nearest_nodes:
+
+                nodes_gdf, edges_gdf = self._get_skeleton_nodes_and_edges(
+                    self.road_network, road_node, distance_value, self.distance_type
+                )
+
+                try:
+
+                    new_isopolygon = self._add_buffer_to_isopolygon_skeleton(
+                        nodes_gdf=nodes_gdf,
+                        edges_gdf=edges_gdf,
+                        node_buffer=self.node_buff,
+                        edge_buffer=self.edge_buff,
+                    )
+                    isopolygons.loc[road_node, "ID_" + str(distance_value)] = (
+                        new_isopolygon
+                    )
+
+                except (
+                    AttributeError
+                ):  # Probably trying to catch 'MultiPolygon' object has no attribute 'exterior' from _inflate_skeleton, but it wasn't specified in the code before
+
+                    logging.info(f"problem with node {road_node}")  # stops execution
+
+        return isopolygons
+
+    @staticmethod
+    def _add_buffer_to_isopolygon_skeleton(
+        nodes_gdf: GeoSeries,
+        edges_gdf: GeoSeries,
+        node_buffer: float,
+        edge_buffer: float,
+    ) -> Polygon:
+        """The nodes and edges form the "skeleton" of the isopolygon. This function turns that skeleton into
+        a polygon by buffering and merging the geometries of the nodes and edges.
+
+        Returns
+        -------
+        shapely.geometry.Polygon
+            A polygon representing the merged buffer zones
+        Raises
+        ------
+        ValueError
+            If edge_buffer is less than or equal to 0
+        AttributeError
+            If the unary_union results in two (or more) disconnected polygons
+        Notes
+        -----
+        - The function is sensitive to buffer size values. If they are too large relative to the distances between
+        nodes, the buffer could be so large that unintended areas are included by mistake (like nodes that had been
+        previously excluded).
+        - Input geometries should be in a projected CRS, not geographic CRS, to ensure
+          accurate buffer calculations."""
+
+        if edge_buffer <= 0:
+            raise ValueError("The parameter edge_buffer must be greater than 0.")
+
+        # creates a circle with radius node_buffer around each node
+        disks = nodes_gdf.buffer(node_buffer).geometry
+
+        # creates a buffer around each edge. For example, if the edge is a line between
+        # two points, it creates a sort of 2-d "cylinder" of "radius" edge_buffer
+        # and semicircles of radius edge_buff at the end points
+        cylinders = edges_gdf.buffer(edge_buffer).geometry
+
+        all_geometries = list(disks) + list(cylinders)
+
+        # Merges all_geometries into a single unified geometry, removing overlaps and simplifying the resulting shape
+        geometric_union = gpd.GeoSeries(all_geometries).unary_union
+
+        # Removes any interior boundaries (holes) that geometric_union might have had
+        buffer_polygon = Polygon(geometric_union.exterior)
+
+        return buffer_polygon
+
+    @staticmethod
+    def _get_skeleton_nodes_and_edges(
+        road_network: nx.MultiDiGraph,
+        center_node: int,
+        distance_value: int,
+        distance_type: str,
+    ) -> tuple[gpd.GeoSeries, gpd.GeoSeries]:
+        """Get nodes and edges within a specified distance from a certain node in a road network.
+        This will be the "skeleton" of the isopolygon.
+
+        Parameters:
+            road_network (nx.MultiDiGraph): The road network.
+            center_node (int): The node from which to measure the distance.
+            dist_value (int): The distance value.
+            distance_type (str): The type of distance (e.g., 'length').
+
+        Returns:
+            nodes_gdf: a GeoSeries of the nodes with their osmid and geometry.
+            edges_gdf: a GeoSeries of the geometry of the edges.
+
+
+        Notes
+        -----
+        If an edge (u,v) doesn't have geometry data in the road_network, edges_gdf contains
+        a straight line from u to v.
+
+        If no edges are found (for example, if all other nodes are too far away from center_node),
+        edges_gdf is an empty dataframe."""
+
+        subgraph = nx.ego_graph(
+            road_network, center_node, radius=distance_value, distance=distance_type
+        )
+
+        try:
+            nodes_gdf, edges_gdf = ox.graph_to_gdfs(subgraph)
+            return (
+                nodes_gdf.loc[:, "geometry"],
+                edges_gdf.loc[:, "geometry"].reset_index(),
+            )
+
+        except ValueError:  # if no edges are found
+            nodes_gdf = ox.graph_to_gdfs(subgraph, edges=False)
+            return (
+                nodes_gdf.loc[:, "geometry"],
+                gpd.GeoSeries([], name="geometry", crs=nodes_gdf.crs),
+            )
+
+
+class OsmIsopolygonCalculatorAlternative(IsopolygonCalculator):
+    """This implementation of IsopolygonCalculator uses OpenStreetMap data to calculate isopolygons."""
+
+    def __init__(
+        self,
+        facilities_lon_lat: DataFrame,
+        distance_type: str,
+        distance_values: list[int],
+        road_network: MultiDiGraph,
+        buffer: float = 50,  # in meters
+    ):
+        super().__init__(facilities_lon_lat, distance_type, distance_values)
+        self.road_network = road_network
+        self.buffer = buffer
+
+        # For each facility, find the nearest node in the road network
+        self.nearest_nodes = ox.distance.nearest_nodes(
+            G=self.road_network,
+            X=self.facilities_longitude_array,
+            Y=self.facilities_latitude_array,
+        )
+
+    def calculate_isopolygons(self) -> DataFrame:
+        """
+        Calculate isopolygons for each facility at different distances (distance_values).
+
+        An isopolygon represents the area that can be reached within a specific distance/time from a facility
+        using the road network.
+
+        N.B.: distances will be computed with respect to the nearest node to the facility,
+        not the facility itself.
+
+        """
+
+        index = pd.Index(self.nearest_nodes, name="nearest_node")
+        columns = [f"ID_{d}" for d in self.distance_values]
+        isopolygons = pd.DataFrame(index=index, columns=columns)
+
+        for distance_value in self.distance_values:
+
+            # Get skeletons for all nodes at this distance value
+            skeletons = [
+                self._get_skeleton_nodes_and_edges(
+                    self.road_network, node, distance_value, self.distance_type
+                )
+                for node in self.nearest_nodes
+            ]
+
+            # "Inflate" skeletons to isopolygons at this distance value
+            isopolygon_series = pd.Series(skeletons).apply(
+                lambda x: ox.utils_geo.buffer_geometry(x, self.buffer)
+            )
+
+            # Assign column with isopolygons to the dataframe
+            isopolygons[f"ID_{distance_value}"] = isopolygon_series.values
+
+        return isopolygons
+
+    @staticmethod
+    def _get_skeleton_nodes_and_edges(
+        road_network: nx.MultiDiGraph,
+        center_node: int,
+        distance_value: int,
+        distance_type: str,
+    ):
+        """
+        Get nodes and edges within distance_value from a node in the road network, and return
+        the union of their geometries. This will be the "skeleton" of the isopolygon.
+
+        Parameters:
+            road_network (nx.MultiDiGraph): The road network.
+            center_node (int): The node from which to measure the distance.
+            distance_value (int): The distance value.
+            distance_type (str): The type of distance (e.g., 'length').
+
+        Returns:
+            The union of the geometries of the nodes and edges within the specified distance from center_node.
+        """
+        subgraph = nx.ego_graph(
+            road_network, center_node, radius=distance_value, distance=distance_type
+        )
+
+        try:
+            nodes_gdf, edges_gdf = ox.graph_to_gdfs(subgraph)
+
+            skeleton = (edges_gdf.geometry.union_all()).union(
+                nodes_gdf.geometry.union_all()
+            )
+
+        except ValueError:  # if no edges are found
+            nodes_gdf = ox.graph_to_gdfs(subgraph, edges=False)
+
+            skeleton = nodes_gdf.geometry.union_all()
+
+        return skeleton
+
+
+class MapboxIsopolygonCalculator(IsopolygonCalculator):
+
+    def __init__(
+        self,
+        facilities_lon_lat: DataFrame,
+        distance_type: str,
+        distance_values: list[int],
+        route_profile: str,  # ?
+        mapbox_api_token: str,
+    ):
+        super().__init__(facilities_lon_lat, distance_type, distance_values)
+        self.route_profile = route_profile
+        self.mapbox_api_token = mapbox_api_token
+
+    def calculate_isopolygons(self) -> DataFrame: ...
