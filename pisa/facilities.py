b8865f54854e36941596eceb77baf9b7367b231e
Philipp Kollenda <pkollenda@gmail.com>
2025-04-02 22:06:56 +0200

    Philipp/new facilities class (#61)
    * add facilities class
    
    * add tests for facilities class
    
    * vectorized method _get_existing_facilities_osm in facilities.py
    
    * added docstring to _get_existing_facilities_osm in facilities.py
    
    * refactor: implement comments catalina
    
    * fix: change default osm tag to amenity and re-insert geometry to potential facility gdf
    
    * calculate centroid on flattened projection and re-project to original in facilities.py and tests. also, test results should be taken from mocked fake_facilities and not hardcoded.
    
    * remove projection from centroid calculation in facilities and tests
    
    * remove redundant test and outdated comment
    
    * drop geometry column from potential facilities and adjust tests
    
    * fix tests to use x, y for nodes and centroid for way
    
    * tests: remove outdated variable and unnecessary test
    
    * update github actions
    
    * update github actions
    
    * update poetry version in workflow
    
    * fix poetry version in workflow to 2.1.1
    
    * Revert "fix poetry version in workflow to 2.1.1"
    
    This reverts commit 05aad1df6024a4314a5f7cc17262e05e2d3c47f1.
    
    * fix: install poetry from abatilo action, which sets up own virtual environments
    
    * Revert "Merge branch 'philipp/fix_workflows' into philipp/new_facilities_class"
    
    This reverts commit ddeab7804045bfc059a7968d3ad2695719a14dae, reversing
    changes made to c2b11f7082f6b7065b8f304af4c722d9995c06c9.
    
    * correctly merge dev into facilities branch to run tests successfully
    
    ---------
    
    Co-authored-by: Catalina <dcossaogh@use.startmail.com>

---
+++ pisa/facilities.py
@@ -0,0 +1,109 @@
+import logging
+from dataclasses import dataclass, field
+
+import geopandas as gpd
+import numpy as np
+import osmnx as ox
+from pandas import DataFrame
+from shapely import MultiPolygon, Polygon
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class Facilities:
+    """Get existing and potential facility locations for given administrative area"""
+
+    administrative_area: Polygon | MultiPolygon
+    data_src: str = "osm"
+    osm_tags: dict = field(default_factory=lambda: {"amenity": "hospital"})
+
+    def get_existing_facilities(self) -> DataFrame:
+        """Get facilities from specified data source"""
+        if self.data_src == "osm":
+            return self._get_existing_facilities_osm(
+                administrative_area=self.administrative_area, osm_tags=self.osm_tags
+            )
+        raise NotImplementedError(f"Data source '{self.data_src}' not implemented")
+
+    @staticmethod
+    def _get_existing_facilities_osm(
+        osm_tags: dict, administrative_area: Polygon | MultiPolygon
+    ) -> DataFrame:
+        """
+        Fetches existing facilities from OpenStreetMap (OSM) within a specified administrative area.
+        Parameters
+        ----------
+        osm_tags : dict
+            Dictionary of OSM tags to filter facilities (e.g., {'amenity': 'school'})
+        administrative_area : Polygon | MultiPolygon
+            Geographic area within which to search for facilities, defined as a shapely Polygon
+            or MultiPolygon object
+        Returns
+        -------
+        pandas.DataFrame
+            DataFrame containing facilities information with columns:
+            - osmid (index): OSM ID of the facility
+            - longitude: Longitude of facility
+            - latitude: Latitude of facility
+        """
+
+        logger.info(f"Retrieving existing facilities with tags {osm_tags} using OSM.")
+
+        # retrieves facilities GeodataFrame from osm
+        facilities_gdf = ox.features_from_polygon(
+            polygon=administrative_area, tags=osm_tags
+        )
+
+        # from the geometry column create longitude and latitude columns,
+        # independently on whether the element is node or way
+        facilities_gdf["longitude"] = facilities_gdf.geometry.centroid.x
+        facilities_gdf["latitude"] = facilities_gdf.geometry.centroid.y
+
+        # reset multiindex and drop some columns. It becomes a DataFrame
+        facilities_df = facilities_gdf.reset_index().drop(
+            columns=["element", "amenity", "geometry"]
+        )
+
+        logger.info("Successfully retrieved existing facilities from OSM.")
+
+        # index facilities_df is the OSMID of the facility.
+        # We don't strictly need it, but it could be useful for debugging.
+        return facilities_df.set_index("id").rename_axis("osmid")
+
+    def estimate_potential_facilities(self, spacing: float) -> gpd.GeoDataFrame:
+        """Create grid of potential facility locations
+
+        Create a grid of evenly spaced points within the given GeoDataFrame.
+
+        Arguments:
+            spacing: The distance between the points in coordinate units.
+
+        Returns:
+            GeoDataFrame containing potential facility locations
+
+        """
+        # Get the bounds of the polygon
+        minx, miny, maxx, maxy = self.administrative_area.bounds
+
+        # Square around the polygon with the min, max polygon bounds
+        x_coords = list(np.arange(np.floor(minx), np.ceil(maxx + spacing), spacing))
+        y_coords = list(np.arange(np.floor(miny), np.ceil(maxy + spacing), spacing))
+
+        # Now generate the entire grid
+        mesh = np.meshgrid(x_coords, y_coords)
+        grid = gpd.GeoDataFrame(
+            data={
+                "longitude": mesh[0].flatten(),
+                "latitude": mesh[1].flatten(),
+            },
+            geometry=gpd.points_from_xy(mesh[0].flatten(), mesh[1].flatten()),
+        )
+
+        # Clip the grid to the admin area boundaries
+        grid = gpd.clip(grid, self.administrative_area)
+        grid = grid.drop(columns=["geometry"])
+        grid = grid.reset_index(drop=True).reset_index().rename(columns={"index": "ID"})
+
+        return grid
