fcb763c88f1cb27bf1328f8a4bb97014fd6b0c64
Catalina <dcossaogh@use.startmail.com>
2025-04-12 10:46:45 +0200

    adjusting OsmRoadNetwork in case the user wants to adjust the fallback speed

--- pisa/constants.py
+++ pisa/constants.py
@@ -1,5 +1,10 @@
 """Constants used throughout the PISA package."""
 
+VALID_DISTANCE_TYPES = {"length", "travel_time"}
+
 VALID_MODES_OF_TRANSPORT = {"driving", "walking", "cycling"}
 
-VALID_DISTANCE_TYPES = {"length", "travel_time"}
+
+DEFAULT_FALLBACK_DRIVING_SPEED = 50
+DEFAULT_FALLBACK_WALKING_SPEED = 4
+DEFAULT_FALLBACK_CYCLING_SPEED = 15
--- pisa/osm_road_network.py
+++ pisa/osm_road_network.py
@@ -1,13 +1,25 @@
+import logging
+
 import networkx as nx
 import osmnx as ox
 from shapely import MultiPolygon, Polygon
 
+from pisa.constants import (
+    DEFAULT_FALLBACK_CYCLING_SPEED,
+    DEFAULT_FALLBACK_DRIVING_SPEED,
+    DEFAULT_FALLBACK_WALKING_SPEED,
+)
 from pisa.utils import _validate_distance_type, _validate_mode_of_transport
+
+logger = logging.getLogger(__name__)
 
 
 class OsmRoadNetwork:
     """
     Class to retrieve and process OpenStreetMap road network data.
+
+    TODO: adjust docstring, explain what fallback speed means (check
+    docstring of ox.add_edge_speeds)
 
     Parameters
     ----------
@@ -16,11 +28,10 @@ class OsmRoadNetwork:
 
     mode_of_transport: str
         The mode of transport for which the road network is required.
-        Valid inputs : 'driving', 'walking', 'cycling'
 
     distance_type: str
         The type of distance to be calculated.
-        Valid inputs : 'length', 'travel_time'
+
     """
 
     def __init__(
@@ -28,6 +39,7 @@ class OsmRoadNetwork:
         admin_area_boundaries: Polygon | MultiPolygon,
         mode_of_transport: str,  # must be an element of VALID_MODES_OF_TRANSPORT
         distance_type: str,  # must be an element of VALID_DISTANCE_TYPES
+        fallback_speed: str = None,
     ):
         # validate distance type
         self.distance_type = _validate_distance_type(distance_type)
@@ -35,18 +47,28 @@ class OsmRoadNetwork:
         # validate mode of transport
         mode_of_transport = _validate_mode_of_transport(mode_of_transport)
 
-        # process mode of transport
-        self.network_type, self.default_speed = self._get_network_type(mode_of_transport)
+        self.network_type = self._set_network_type(mode_of_transport)
 
         self.admin_area_boundaries = admin_area_boundaries
 
+        if self.distance_type == "travel_time":
+            self.fallback_speed = self._set_fallback_speed(fallback_speed)
+
+        logger.info(
+            """OSM road network set with parameters 
+                    
+                    network_type f"{self.network_type}",
+                    distance_type f"{self.distance_type}",                    
+            """
+        )
+
     def get_osm_road_network(self) -> nx.MultiDiGraph:
-        """Process the OSM road network."""
+        """Returns the processed OSM road network."""
 
         road_network = self._download_osm_road_network()
 
         if self.distance_type == "travel_time":
-            return self._add_time_to_edges(road_network, self.default_speed)
+            return self._add_time_to_edges(road_network, self.fallback_speed)
 
         return road_network
 
@@ -58,21 +80,28 @@ class OsmRoadNetwork:
         )
 
     @staticmethod
-    def _get_network_type(mode_of_transport: str) -> tuple[str, int]:
-        """Set valid network type and default speed based on input"""
-        network_mapping = {
-            "driving": ("drive", 50),
-            "walking": ("walk", 4),
-            "cycling": ("bike", 15),
-        }
-        return network_mapping[mode_of_transport]
+    def _set_network_type(mode_of_transport: str) -> str:
+        """TODO: modify only the strings (e.g. driving -> drive)"""
+
+        ...
+
+    ## FYI: next functions are only used when calculating isochrones, not distances
 
     @staticmethod
     def _add_time_to_edges(
-        road_network: nx.MultiDiGraph, default_speed: int
+        road_network: nx.MultiDiGraph, road_speed: int
     ) -> nx.MultiDiGraph:
-        """Add travel time edge attribute and change unit to minutes"""
-        road_network = ox.add_edge_speeds(road_network, fallback=default_speed)
+        """Add travel time edge attribute and change unit to minutes
+
+        TODO:
+        - add tests!!! Important to understand what fallback speed means in practice
+
+        - specify units (kph?)
+
+        - add docstring
+
+        """
+        road_network = ox.add_edge_speeds(road_network, fallback=road_speed)
         road_network = ox.add_edge_travel_times(road_network)
 
         time = nx.get_edge_attributes(road_network, "travel_time")
@@ -80,3 +109,48 @@ class OsmRoadNetwork:
         nx.set_edge_attributes(road_network, time_in_minutes, "travel_time")
 
         return road_network
+
+    def _set_fallback_speed(self, fallback_speed: int | None) -> int:
+        """TODO:
+
+        If user wrote in a fallback speed, make sure it sort of makes sense
+        (see function _validate_speed_input)
+
+        Otherwise, return default fallback speed for the network type
+
+        """
+
+        if fallback_speed is not None:
+            return self._validate_fallback_speed_input(
+                fallback_speed, self.network_type
+            )
+
+        ### TODO: return the default for the network_type
+
+        ...
+
+    @staticmethod
+    def _validate_fallback_speed_input(fallback_speed: int, network_type: str) -> int:
+        """TODO: do some "common-sense" checks if the user wants to override the
+        default fallback speeds
+
+        User should not input "very" high or low speeds proportionately to the default_fallback_speed for the network_type.
+        Examples:
+        - 40 might be ok for a driving network, but not for a walking network
+        - 500 is inadmissible always
+
+        If the road_speed makes sense, return it. Else, raise errors
+
+        Important: clarify units (I think it's km/h, but please double check)
+
+        TODO: add tests for corner cases
+
+        """
+        # raise errors if values don't make sense
+
+        # else:
+
+        logger.info("""Setting fallback speed to f"{fallback_speed}" """)
+
+        return fallback_speed
+        ...
