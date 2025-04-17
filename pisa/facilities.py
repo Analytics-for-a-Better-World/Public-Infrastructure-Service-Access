4820bb4fb7629b3fb3543156a0549ce4dabeb439
anoukb27 <anoukbaake@gmail.com>
2025-04-17 12:55:28 +0200

    catch insufficientresponseerror from ox.features_from_polygon

--- pisa/facilities.py
+++ pisa/facilities.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass, field
 import geopandas as gpd
 import numpy as np
 import osmnx as ox
+from osmnx._errors import InsufficientResponseError
 from pandas import DataFrame
 from shapely import MultiPolygon, Polygon
 
@@ -56,9 +57,12 @@ class Facilities:
         logger.info(f"Retrieving existing facilities with tags {osm_tags} using OSM.")
 
         # retrieves facilities GeodataFrame from osm
-        facilities_gdf = ox.features_from_polygon(
-            polygon=admin_area_boundaries, tags=osm_tags
-        )
+        try:
+            facilities_gdf = ox.features_from_polygon(
+                polygon=admin_area_boundaries, tags=osm_tags
+            )
+        except InsufficientResponseError as e:
+            raise ValueError(f"Error retrieving facilities from OSM: {e}")
 
         # from the geometry column create longitude and latitude columns,
         # independently on whether the element is node or way
