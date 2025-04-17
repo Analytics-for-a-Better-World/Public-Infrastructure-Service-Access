bf55a74b755e3b8c31c9b68068d9369f391994b7
anoukb27 <anoukbaake@gmail.com>
2025-04-17 10:38:57 +0200

    Restructure streamlit app

---
+++ pisa_app/tab_country_data.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from pisa.administrative_area import AdministrativeArea
+import pycountry
+
+
+countries = sorted([country.name for country in list(pycountry.countries)])
+
+
+def country_data(ss):
+    st.subheader("Country Data")
+    st.selectbox("Country:", countries, key="country")
+    st.selectbox("Administrative Level Granularity:", [0, 1, 2], key="level")
+    ss.submitted_country = st.button("Submit Country")
+
+    if ss.submitted_country:
+        ss.adm_area = AdministrativeArea(
+            ss.country, ss.level
+        )
+        st.write("Choose administrative area")
+        ss.adm_areas_str = ss.adm_area.get_admin_area_names()
+
+    st.selectbox(
+        "Administrative Areas:",
+        ss.adm_areas_str,
+        key="adm_names",
+    )
+
+    submitted_admarea = st.button("Submit Administrative Area")
+    if submitted_admarea:
+        ss.admin_area_boundaries = ss.adm_area.get_admin_area_boundaries(ss.adm_names)
+        if ss.admin_area_boundaries is not None:
+            st.success(
+                "Administrative area is set. Continue with Facility and Population data."
+            )
---
+++ pisa_app/utils.py
@@ -0,0 +1,56 @@
+import folium
+import streamlit as st
+
+
+def fit_to_bounding_box(
+        folium_map: folium.Map,
+        lon_min: float, lat_min: float,
+        lon_max: float, lat_max: float
+) -> folium.Map:
+    folium_map.fit_bounds(((lat_min, lon_min), (lat_max, lon_max)))
+    return folium_map
+
+
+@st.cache_data(show_spinner=False)
+def get_set_of_available_pyomo_solvers():
+    print('scanning pyomo solvers...', end='', flush=True)
+    import subprocess
+    shell_command = "pyomo help --solvers"
+    output = subprocess.check_output(shell_command, shell=True).decode()
+    print(' done.')
+    return {
+        line.strip()[1:]
+        for line in output.split()
+        if line.strip().startswith("+") and not line.strip().endswith(")")
+    }
+
+
+def get_available_solvers():
+    candidate_solvers = {
+        'appsi_gurobi',
+        'appsi_highs',
+        'cbc',
+        'cplex',
+        'cplex_direct',
+        'glpk',
+        'gurobi',
+        'gurobi_direct',
+    }
+    scanned = get_set_of_available_pyomo_solvers()
+    return sorted(candidate_solvers & scanned)
+
+
+def init_session_state(ss):
+    defaults = {
+        'available_solvers': get_available_solvers(),
+        'adm_area': None,
+        'adm_areas_str': [],
+        'fac_map_obj': folium.Map(location=(51.509865, -0.118092), zoom_start=1),
+        'pop_map_obj': folium.Map(location=(51.509865, -0.118092), zoom_start=1),
+        'existing_facilities_df': None,
+        'potential_facilities_gdf': None,
+        'population_gdf': None,
+    }
+    for key, value in defaults.items():
+        if key not in ss:
+            ss[key] = value
