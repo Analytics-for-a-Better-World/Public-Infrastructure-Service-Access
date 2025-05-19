9a413ee324e7367547aa6948feef6969e752ebf7
Philipp Kollenda <pkollenda@gmail.com>
2025-05-19 21:52:58 +0200

    docs: include example notebook and pisa apis

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
 ]
-autoapi_dirs = ["../gpbp"]
+autoapi_dirs = ["../gpbp", "../pisa"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
